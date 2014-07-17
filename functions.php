5134c46a3d8f7339e69a0708fb8853e68caa78a3
mkdgr <mkd@mkdgr.com>
2014-07-18 02:13:17 +0300

    Added infinite scroll

--- functions.php
+++ functions.php
@@ -15,3 +15,44 @@ function tokenText($str) {
     }
     return $object;
 }
+
+
+function infinite_scroll(){
+    wp_register_script(
+        'infinite_scrolling',/
+        get_template_directory_uri().'/js/jquery.infinitescroll.min.js',
+        array('jquery'),
+        null,
+        true
+    );
+
+    if(!is_singular()){
+        wp_enqueue_script('infinite_scrolling');        
+    }
+}
+
+function set_infinite_scrolling(){
+
+    if(!is_singular()){
+
+?>    
+        <script type="text/javascript">            
+            var inf_scrolling = {                
+                loading:{
+                    img: "<?php echo get_template_directory_uri(); ?>/images/ajax-loading.gif",
+                    msgText: "Loading next posts....",
+                    finishedMsg: "Posts loaded!!",
+                },
+                "nextSelector":".Events a:last-child",
+                "navSelector":".Events a:last-child",
+                "itemSelector":"article.Event",
+                "contentSelector":"#Events"
+            };
+
+            jQuery(inf_scrolling.contentSelector).infinitescroll(inf_scrolling);
+        </script>        
+    <?php
+    }
+}
+add_action( 'wp_footer', 'set_infinite_scrolling',100 );
+?>
