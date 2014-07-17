eba4c189e0da80ca336df9a119ec6c48fc112524
mkdgr <mkd@mkdgr.com>
2014-07-18 02:00:56 +0300

    added get_header
    for wordpress theme standart

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title(); ?></title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6110492/691904/css/fonts.css"/>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
-
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-    <?php wp_head(); ?>
-
-    <?php //include_once("analyticstracking.php"); //Add your analytics code in that file and uncomment this line ?>
-
-</head>
-<body <?php body_class(); ?>>
-
-<?php include 'icons.php'; ?>
-
+<?php get_header(); ?>
 <div class="Page">
     <div class="Events">
 
